6efd2689f0d7134c60cfa95b0efb21ef4666da22
sedaaydin360drc <seda.aydin@360drctech.com>
2024-01-24 04:04:23 +0300

    first commit

---
+++ src/ScheduleServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Sedaaydin360drc\Automaintenance;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Console\Scheduling\Schedule;
+
+class ScheduleServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->app->afterResolving(Schedule::class, function (Schedule $schedule) {
+            if(env('MAINTENANCE_MODE_DOWN') && env('MAINTENANCE_MODE_DOWN') !== null && env('MAINTENANCE_MODE_UP') && env('MAINTENANCE_MODE_UP') !== null) {
+                $schedule->command('down')
+                    ->cron(env('MAINTENANCE_MODE_DOWN'))
+                    ->timezone('Europe/Istanbul')
+                    ->evenInMaintenanceMode();
+                $schedule->command('up')
+                    ->cron(env('MAINTENANCE_MODE_UP'))
+                    ->timezone('Europe/Istanbul')
+                    ->evenInMaintenanceMode();
+            }
+        });
+    }
+}
